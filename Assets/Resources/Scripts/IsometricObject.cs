558efa7db6166ff7fcb3ae8ca8df5df1aadfccd5
Adrian Wong <Adrian Wong>
2017-02-11 12:46:10 -0800

    implement height and refactor

--- Assets/Resources/Scripts/IsometricObject.cs
+++ Assets/Resources/Scripts/IsometricObject.cs
@@ -6,23 +6,52 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class IsometricObject : MonoBehaviour {
 
-        public float    isoScale;
-        public Vector3  isoPosition;
-        private Vector3 realPosition;
-
-    // Use this for initialization
-    void Start ()
+    [Serializable]
+    public class IsoScale
     {
-        realPosition = new Vector3();
+        public float xYFactor;      // The size of one isometric square in Unity Space
+        public float heightFactor;  // The size of one unit of height in Unity Space
     }
 
-    // Update is called once per frame
-    void Update ()
+    [Serializable]
+    public class IsoPosition
+    {
+        public float isoX;      // Object's X position in Isometric Space
+        public float isoY;      // Object's Y position in Isometric Space
+        public float isoHeight; // Object's Height in Isometric Space
+        public float drawDepth; // Object's Z position in Unity Space for drawing layers 
+    }
+
+    // Declare Fields
+    public IsoScale     isoScale;
+    public IsoPosition  isoPosition;
+    private Vector3     realPosition; // Object's transform position in Unity Space
+
+    // Use this for initialization
+    void Start()
+    {
+        realPosition = new Vector3();
+        UpdateRealPosition();
+    }
+
+    void OnValidate ()
+    {
+        UpdateRealPosition();
+    }
+
+    // Set object's real position based on its position in Isometric Space
+    public void UpdateRealPosition()
     {
         realPosition.Set(
-            isoScale * isoPosition.x - isoScale * isoPosition.y,                //X
-            0.5f * isoScale * isoPosition.x + 0.5f * isoScale * isoPosition.y,  //Y
-            isoPosition.z                                                       //Z
+            //Set X
+            isoScale.xYFactor * isoPosition.isoX -
+            isoScale.xYFactor * isoPosition.isoY,
+            //Set Y
+            0.5f * isoScale.xYFactor * isoPosition.isoX +
+            0.5f * isoScale.xYFactor * isoPosition.isoY +
+            isoScale.heightFactor * isoPosition.isoHeight,
+            //Set Z
+            isoPosition.drawDepth
             );
 
         gameObject.transform.position = realPosition;
