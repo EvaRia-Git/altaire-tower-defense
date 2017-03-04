668a7ce9c08f43a19826c398dfd8df3a336ae106
Adrian Wong <Adrian Wong>
2017-03-04 13:58:29 -0800

    Flatten 2D Tile Array to fix serializing bug.

--- Assets/Resources/Scripts/IsoGrid.cs
+++ Assets/Resources/Scripts/IsoGrid.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class IsoGrid : MonoBehaviour {
 
-    //Scale of this Iso Grid
+    // Scale of this Iso Grid
     [Serializable]
     public class IsoScale
     {
@@ -14,22 +14,39 @@ public class IsoGrid : MonoBehaviour {
         public float heightFactor;  // The size of one unit of height in Unity Space
     }
 
-    //Length and width of the grid.
+    // Length and width of the grid.
     [Serializable]
     public class GridSize
     {
-        //Default to one
+        // Default to one
+        public int x = 1;
+        public int y = 1;
+        
+    }
+
+    // Old length and width of the grid.
+    // Stored to keep track of indices when iterating through the refresh loops
+    [Serializable]
+    public class OldGridSize
+    {
+        // Default to one
         public int x = 1;
         public int y = 1;
     }
 
-    //Declare fields
+    // Declare fields
     public IsoScale isoScale = new IsoScale();
     public GridSize gridSize = new GridSize();
-    public bool refreshGrid = true;             // Used as a button in the editor. Simple but hacky.
-                                                // Defaults to true for editor-only initialization.
+    [HideInInspector]
+    public OldGridSize oldGridSize = new OldGridSize();
 
-    private GameObject[,] Tiles;                // Contains a reference to every tile in the grid.
+    // Used as a button in the editor. Simple but hacky.
+    // Defaults to true for editor-only initialization.
+    public bool refreshGrid = true;             
+
+    // Contains a reference to every tile in the grid.
+    [HideInInspector]
+    public GameObject[] Tiles;
 
     void Update()
     {
@@ -47,31 +64,30 @@ public class IsoGrid : MonoBehaviour {
         // First time initialization.
         if (Tiles == null)
         {
-            Tiles = new GameObject[1, 1];
+            Tiles = new GameObject[1];
         }
 
         // We create a temporary grid with the new size.
         // This is so we can move existing tiles to the new grid, since 2D Arrays are not expandable.
-        GameObject[,] tempGrid = new GameObject[X, Y];
+        GameObject[] newGrid = new GameObject[X*Y];
 
         // First loop destroys any old tiles that are outside the border of the new grid.
-        for (int i = 0; i < Tiles.GetLength(0); i++)
+        for (int i = 0; i < oldGridSize.x; i++)
         {
-            for (int j = 0; j < Tiles.GetLength(1); j++)
+            for (int j = 0; j < oldGridSize.y; j++)
             {
                 if (i >= X || j >= Y)
                 {
-                    DestroyImmediate(Tiles[i, j]);
+                    DestroyImmediate(Tiles[j * oldGridSize.x + i]);
                 }
             }
         }
-
         // Second loop moves remaining existing tiles from the old grid to the new grid.
-        for (int i = 0; i < Math.Min(X, Tiles.GetLength(0)); i++)
+        for (int i = 0; i < Math.Min(X, oldGridSize.x); i++)
         {
-            for (int j = 0; j < Math.Min(Y, Tiles.GetLength(1)); j++)
+            for (int j = 0; j < Math.Min(Y, oldGridSize.y); j++)
             {
-                tempGrid[i, j] = Tiles[i, j];
+                    newGrid[j * X + i] = Tiles[j * oldGridSize.x + i];
             }
         }
 
@@ -81,30 +97,32 @@ public class IsoGrid : MonoBehaviour {
             for (int j = 0; j < Y; j++)
             {
                 // We put new tiles in any empty space on the new grid.
-                if (tempGrid[i, j] == null)
+                if (newGrid[j * X + i] == null)
                 {
-                    tempGrid[i, j] = new GameObject("Tile-" + i + "-" + j);
-                    tempGrid[i, j].transform.SetParent(transform);
-                    tempGrid[i, j].AddComponent<IsoTile>();
-                    tempGrid[i, j].GetComponent<IsometricObject>().isoPosition.drawDepth += i + j;
+                    newGrid[j * X + i] = new GameObject("Tile-" + i + "-" + j);
+                    newGrid[j * X + i].transform.SetParent(transform);
+                    newGrid[j * X + i].AddComponent<IsoTile>();
+                    newGrid[j * X + i].GetComponent<IsometricObject>().isoPosition.drawDepth += i + j;
 
                     // Since we are controlling the position of tiles using the grid, 
                     // we hide its manual controls in the inspector.
-                    tempGrid[i, j].GetComponent<IsometricObject>().hideFlags = HideFlags.HideInInspector;
+                    newGrid[j * X + i].GetComponent<IsometricObject>().hideFlags = HideFlags.HideInInspector;
                 }
 
                 // All the tiles in the grid are set to the scale defined by the grid.
                 // Then each tile is moved to its correct spot in the isometric world and its position updated.
-                tempGrid[i, j].GetComponent<IsometricObject>().isoScale.xYFactor = isoScale.xYFactor;
-                tempGrid[i, j].GetComponent<IsometricObject>().isoScale.heightFactor = isoScale.heightFactor;
-                tempGrid[i, j].GetComponent<IsometricObject>().isoPosition.isoX = i;
-                tempGrid[i, j].GetComponent<IsometricObject>().isoPosition.isoY = j;
-                tempGrid[i, j].GetComponent<IsometricObject>().UpdateRealPosition();
+                newGrid[j * X + i].GetComponent<IsometricObject>().isoScale.xYFactor = isoScale.xYFactor;
+                newGrid[j * X + i].GetComponent<IsometricObject>().isoScale.heightFactor = isoScale.heightFactor;
+                newGrid[j * X + i].GetComponent<IsometricObject>().isoPosition.isoX = i;
+                newGrid[j * X + i].GetComponent<IsometricObject>().isoPosition.isoY = j;
+                newGrid[j * X + i].GetComponent<IsometricObject>().UpdateRealPosition();
             }
         }
 
         // Now that we are done reconstructing the grid, we assign it back to the original reference.
-        Tiles = tempGrid;
+        oldGridSize.x = X;
+        oldGridSize.y = Y;
+        Tiles = newGrid;
     }
 
 }
